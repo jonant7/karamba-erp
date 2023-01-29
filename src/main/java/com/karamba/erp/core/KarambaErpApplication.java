bc9049cd0236397afadf2ec3490de7757de59f79
Jonathan Navarro <jonathan.navarro.j@gmail.com>
2023-01-28 21:48:05 -0500

    Initial commit Karamba ERP

--- src/main/java/com/karamba/erp/core/KarambaErpApplication.java
+++ src/main/java/com/karamba/erp/core/KarambaErpApplication.java
@@ -1,4 +1,4 @@
-package com.karamba.erp;
+package com.karamba.erp.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/karamba/erp/core/KarambaErpApplicationTests.java
+++ src/test/java/com/karamba/erp/core/KarambaErpApplicationTests.java
@@ -1,4 +1,4 @@
-package com.karamba.erp;
+package com.karamba.erp.core;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
